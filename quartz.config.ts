64771b744fccca2e27fe26f8abca9369cb4b1985
migsaenz <saenzm4307@gmail.com>
2025-01-03 02:14:09 -0800

    Quartz sync: Jan 3, 2025, 2:14 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "mig",
+    baseUrl: "migsaenz.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     generateSocialImages: false,
